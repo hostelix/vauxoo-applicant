fac156591b504e75e9cbd5333e5469b12174bd64
Israel Lugo <hostelixisrael@gmail.com>
2016-02-18 00:52:32 -0430

    [MOD] add method is_prime

--- primes.py
+++ primes.py
@@ -1,16 +1,35 @@
 """
-Your module documentation here
+Module PrimeClass
 """
 
 
 class PrimeClass(object):
     """
-    Your class documentation here
+    methods:
+        is_prime
     """
 
     def is_prime(self, num_int):
         """
-        Your method documentation here
+            Determine if a number is prime
+            Params:
+            @num_int number integer
+            Variables:
+            @prime bool :is value function return
+            @count int :is count exact division
         """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        num_int = int(num_int)
+        prime = True
+        count = 0
+        
+        if num_int == 1:
+            return not prime
+
+        for n in range(1, num_int+1):
+            if num_int % n == 0:
+                count += 1
+
+            if count > 3:
+                prime = False
+                break
+        return prime
