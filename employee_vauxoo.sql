6e3766a0fd67be57b131e31cfcf75208655aa612
Israel Lugo <hostelixisrael@gmail.com>
2016-02-18 01:28:05 -0430

    [MOD] add insert in DB

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -37,4 +37,32 @@ CREATE TABLE employee_hobby (
 	CONSTRAINT employee_hobby_id_hobby_foreign FOREIGN KEY (id_hobby) 
 		REFERENCES hobbies (id)
 );
+
+
+INSERT INTO employee_department VALUES ( '1','Soporte Tecnico','Reparacion de equipos');
+INSERT INTO employee_department VALUES ( '2','ASOBIES','Control de logistica');
+INSERT INTO employee_department VALUES ( '3','Desarrollo','Desarrollo de aplicaciones');
+INSERT INTO employee_department VALUES ( '4','Maquinaria','Control de maquinari');
+INSERT INTO employee_department VALUES ( '5','Pasantias','Control de pasantias');
+INSERT INTO employee_department VALUES ( '6','Recursos','Recursos Humanos');
+
+INSERT INTO employee VALUES ( 1,'Sandra','Morillo',1);
+INSERT INTO employee VALUES ( 2,'Jose','Castillo',2,1);
+INSERT INTO employee VALUES ( 3,'Victor','Navas',3,1);
+INSERT INTO employee VALUES ( 4,'Julio','Colinas',4,1);
+
+
+INSERT INTO hobbies VALUES ( '1','Ver TV','Ver TV');
+INSERT INTO hobbies VALUES ( '2','Programar','Programar');
+INSERT INTO hobbies VALUES ( '3','Cocinar','Cocinar');
+INSERT INTO hobbies VALUES ( '4','Leer','Leer');
+
+INSERT INTO employee_hobby VALUES ( '1','1');
+INSERT INTO employee_hobby VALUES ( '1','2');
+INSERT INTO employee_hobby VALUES ( '2','1');
+INSERT INTO employee_hobby VALUES ( '2','4');
+INSERT INTO employee_hobby VALUES ( '3','3');
+INSERT INTO employee_hobby VALUES ( '3','2');
+INSERT INTO employee_hobby VALUES ( '4','2');
+INSERT INTO employee_hobby VALUES ( '4','4');
 -- ...
