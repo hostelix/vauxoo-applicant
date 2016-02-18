5f9ac11081c76f093d7c784aaa67b2c16ee38e55
Israel Lugo <hostelixisrael@gmail.com>
2016-02-18 00:49:48 -0430

    [MOD] add new tables employee and employee_department

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -3,10 +3,21 @@
 --       You can create database locally to test it.
 --       Consider add ';' at end sentence.
 
-CREATE TABLE employee (
+CREATE TABLE employee_department (
+	id serial NOT NULL,
+	name character varying(20),
+	description character varying(100),
+	CONSTRAINT id_employee_department_pkey PRIMARY KEY(id)
 );
 
-CREATE TABLE employee_department (
+CREATE TABLE employee (
+	id serial NOT NULL,
+	first_name character varying(20),
+	last_name character varying(20),
+	id_department integer,
+	CONSTRAINT id_employee_pkey PRIMARY KEY(id),
+	CONSTRAINT employee_id_department_foreign FOREIGN KEY (id_department) 
+		REFERENCES employee_department (id)
 );
 
 CREATE TABLE employee_hobby (
